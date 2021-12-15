1204c58a6e701edc364bb18133e1f3312e9d1628
Dat0309 <anhtheboy011@gmail.com>
2021-12-15 16:24:22 +0700

    Update favorite manga

---
+++ MangarReader/app/src/main/java/com/dinhtrongdat/mangareaderapp/viewmodel/FavoriteAct.java
@@ -0,0 +1,16 @@
+package com.dinhtrongdat.mangareaderapp.viewmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dinhtrongdat.mangareaderapp.R;
+
+public class FavoriteAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
